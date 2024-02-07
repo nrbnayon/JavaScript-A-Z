e076041dbc89ea00fbcc233c79d97340c265d11c
nrbnayon <nrbnayon@gmail.com>
2024-02-07 13:27:08 +0600

    Variable Declare

---
+++ First5/variableD.js
@@ -0,0 +1,20 @@
+/**
+ * VARIABLE => Very + Able
+ * 5 Thing to declare a variable
+ *
+ *
+ */
+// 1(var) 2(name) 3(=) 4(value) 5(;)
+var weight = 65;
+
+/**
+ * 3 type of variable => var, let, const
+ * We use let and const
+ * let can change value
+ * but const can't change value
+ *
+ * */
+
+// JavaScript print
+
+console.log(weight);
